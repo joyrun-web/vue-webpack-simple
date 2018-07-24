78721566e1a4f3cb0681df75400c08660a360575
revanli <justin_yufan@163.com>
2018-07-24 23:33:18 +0800

    first commit

---
+++ interfaces/ask.js
@@ -0,0 +1,43 @@
+const execSync = require('child_process').execSync;
+
+module.exports = function askCreator(template = '') {
+  let user = execSync('git config --global user.name', { encoding: 'utf-8' });
+  let email = execSync('git config --global user.email', { encoding: 'utf-8' });
+
+  user = user.trim();
+  email = email.trim();
+
+  return [
+    {
+      type   : 'input',
+      name   : 'name',
+      message: 'Project name',
+      default: template,
+      validate(input) {
+        const done = this.async();
+        if (input.trim().length === 0) {
+          done('project name is empty');
+          return;
+        }
+        done(null, true);
+      }
+    },
+    {
+      type   : 'input',
+      name   : 'Project description',
+      message: 'description',
+      default: 'A Vue.js project'
+    },
+    {
+      type   : 'input',
+      name   : 'License',
+      default: 'MIT'
+    },
+    {
+      type   : 'input',
+      name   : 'author',
+      message: 'author',
+      default: email
+    }
+  ];
+}
---
+++ interfaces/hook.js
@@ -0,0 +1,10 @@
+const exec = require('child_process').execSync
+
+module.exports = {
+   after(meta, module) {
+    console.log('hook after starting')
+
+    return module.runBash(`cd ${meta.dir} && npm i`)
+      .then(() =>  console.log('hook after done'))
+  }
+}
